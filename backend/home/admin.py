d79f90a0d79943203f2ab1ee0b356aabf2c2976b
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-05 06:36:49 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Aerfg
+admin.site.register(Aerfg)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Aerfg(models.Model):
+    'Generated Model'
+    xcvg = models.BigIntegerField()
